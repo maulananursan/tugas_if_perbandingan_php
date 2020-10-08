b39b57c36f49840c2fe0b3e1a2488aa8113a9dff
Maulana Nursan <71365575+maulananursan@users.noreply.github.com>
2020-10-08 12:04:28 +0700

    Add files via upload

---
+++ tugas_if_perbandingan_php.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+	$a=50;
+	$b=60;
+	 
+	 if ($a > $b) {
+	 	echo $a, " lebih besar dari ", $b;
+	 }else if($a < $b){
+	 	echo $a, " lebih kecil dari ", $b;
+	 }else{
+	 echo $a, " sama dengan", $b;
+	}
+	 
+	?>
+</body>
+</html>
